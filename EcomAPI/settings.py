70e6992b518218c62357ae60f95e28edba62bb60
Atef Arman Shishir <atef.arman@yahoo.com>
2022-11-09 14:25:54 +0600

    environment variables

--- EcomAPI/settings.py
+++ EcomAPI/settings.py
@@ -139,8 +139,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "testemailbyshishir@gmail.com"
-EMAIL_HOST_PASSWORD = "yahybcnxnvwepodr"
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 
 
 # Static files (CSS, JavaScript, Images)
